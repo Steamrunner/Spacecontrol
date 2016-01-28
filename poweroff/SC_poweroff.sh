707fdec3252f089b242fd5090e3985f872a2829c
Steamrunner <koen@devoegt.be>
2016-01-28 22:41:17 +0100

    updated to 20sec

--- poweroff/SC_poweroff.sh
+++ poweroff/SC_poweroff.sh
@@ -11,13 +11,20 @@ user=$1
 echo "`date`      logged in user \"$user\" found" >> SC_poweroff.log
 
 #showing the countdown/cancel window
-sudo su $user -c "./SC_poweroff_popup.sh"
+sudo -u $user "./SC_poweroff_popup.sh"
 
 if [ $? = 1 ] ; then
-	echo "`date`      Poweroff canceled" >> SC_poweroff.log
+	echo "`date`      Poweroff canceled by local user" >> SC_poweroff.log
+	/usr/bin/wget -qO- "http://unipi:8080/CMD?Abort=ON" >> /dev/null
 	exit 1
 else
-	echo "`date`      Powering down system" >> SC_poweroff.log
-	/sbin/poweroff
+	abort=`wget -qO- "http://unipi:8080/rest/items/Abort/state"`
+
+	if [ $abort = "OFF" ] ; then
+	        echo "`date`      Powering down system" >> SC_poweroff.log
+		sudo /sbin/shutdown -h now
+	else
+		echo "`date`      Poweroff canceled by SpaceControl" >> SC_poweroff.log
+	fi
 	exit 0
 fi
--- poweroff/SC_poweroff_popup.sh
+++ poweroff/SC_poweroff_popup.sh
@@ -3,16 +3,26 @@
 export DISPLAY=:0.0
 
 (
-echo "99"; echo "#This system will be shutdown by Spacecontrol in 10 seconds.\nClick cancel to abort."; sleep 1
-echo "90"; echo "#This system will be shutdown by Spacecontrol in 9 seconds.\nClick cancel to abort."; sleep 1
-echo "80"; echo "#This system will be shutdown by Spacecontrol in 8 seconds.\nClick cancel to abort."; sleep 1
-echo "70"; echo "#This system will be shutdown by Spacecontrol in 7 seconds.\nClick cancel to abort."; sleep 1
-echo "60"; echo "#This system will be shutdown by Spacecontrol in 6 seconds.\nClick cancel to abort."; sleep 1
-echo "50"; echo "#This system will be shutdown by Spacecontrol in 5 seconds.\nClick cancel to abort."; sleep 1
-echo "40"; echo "#This system will be shutdown by Spacecontrol in 4 seconds.\nClick cancel to abort."; sleep 1
-echo "30"; echo "#This system will be shutdown by Spacecontrol in 3 seconds.\nClick cancel to abort."; sleep 1
-echo "20"; echo "#This system will be shutdown by Spacecontrol in 2 seconds.\nClick cancel to abort."; sleep 1
-echo "10"; echo "#This system will be shutdown by Spacecontrol in 1 seconds.\nClick cancel to abort."; sleep 1
+echo "99"; echo "#This system will be shutdown by Spacecontrol in 20 seconds.\nClick cancel to abort."; sleep 1
+echo "95"; echo "#This system will be shutdown by Spacecontrol in 19 seconds.\nClick cancel to abort."; sleep 1
+echo "90"; echo "#This system will be shutdown by Spacecontrol in 18 seconds.\nClick cancel to abort."; sleep 1
+echo "85"; echo "#This system will be shutdown by Spacecontrol in 17 seconds.\nClick cancel to abort."; sleep 1
+echo "80"; echo "#This system will be shutdown by Spacecontrol in 16 seconds.\nClick cancel to abort."; sleep 1
+echo "75"; echo "#This system will be shutdown by Spacecontrol in 15 seconds.\nClick cancel to abort."; sleep 1
+echo "70"; echo "#This system will be shutdown by Spacecontrol in 14 seconds.\nClick cancel to abort."; sleep 1
+echo "65"; echo "#This system will be shutdown by Spacecontrol in 13 seconds.\nClick cancel to abort."; sleep 1
+echo "60"; echo "#This system will be shutdown by Spacecontrol in 12 seconds.\nClick cancel to abort."; sleep 1
+echo "55"; echo "#This system will be shutdown by Spacecontrol in 11 seconds.\nClick cancel to abort."; sleep 1
+echo "50"; echo "#This system will be shutdown by Spacecontrol in 10 seconds.\nClick cancel to abort."; sleep 1
+echo "45"; echo "#This system will be shutdown by Spacecontrol in 9 seconds.\nClick cancel to abort."; sleep 1
+echo "40"; echo "#This system will be shutdown by Spacecontrol in 8 seconds.\nClick cancel to abort."; sleep 1
+echo "35"; echo "#This system will be shutdown by Spacecontrol in 7 seconds.\nClick cancel to abort."; sleep 1
+echo "30"; echo "#This system will be shutdown by Spacecontrol in 6 seconds.\nClick cancel to abort."; sleep 1
+echo "25"; echo "#This system will be shutdown by Spacecontrol in 5 seconds.\nClick cancel to abort."; sleep 1
+echo "20"; echo "#This system will be shutdown by Spacecontrol in 4 seconds.\nClick cancel to abort."; sleep 1
+echo "15"; echo "#This system will be shutdown by Spacecontrol in 3 seconds.\nClick cancel to abort."; sleep 1
+echo "10"; echo "#This system will be shutdown by Spacecontrol in 2 seconds.\nClick cancel to abort."; sleep 1
+echo "5"; echo "#This system will be shutdown by Spacecontrol in 1 seconds.\nClick cancel to abort."; sleep 1
 ) |
-zenity --progress --title="Spacecontrol Poweroff" --text="This system will be shutdown by Spacecontrol in 10 seconds. \nClick cancel to abort." --percentage=100 --auto-close
+zenity --progress --title="Spacecontrol Poweroff" --text="The Space and this system will be shutdown by Spacecontrol in 20 seconds. \nClick cancel to abort shutdown." --percentage=100 --auto-close
 
