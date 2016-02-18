0f98c4aa705cbc1498e3b18306687a25ba472bdd
steamrunner <koen@devoegt.be>
2016-02-19 00:03:42 +0100

    update test

--- poweroff/test.sh
+++ poweroff/test.sh
@@ -1,20 +1,45 @@
 #!/bin/bash
 
 whoResults=$(who | grep '(:[0-9])')
+
+echo 'whoResults' $whoResults 'end'
+
+if [ "$whoResults" = $'\n' ] ; then
+  echo "FOUND NEWLINE"
+fi
+
+
 readarray -t whoResultLines <<< "$whoResults"
 
 echo "--------------------------------------"
 
 len=${#whoResultLines[@]}
+len=${$whoResultLines[@] | wc -w}
+
+echo $len
+
+if [ ${#whoResultLines[@]} -eq 0 ]; then
+    echo "No errors, hooray"
+else
+    echo "Oops, something went wrong..."
+fi
 
 for (( i=0; i<${len}; i++ ));
-
 do
 	set -- ${whoResultLines[$i]}
 	user=$1
-	display=$5
+	display=${5:1:2}
 	echo $user $display
 done
+
+echo $len
+
+if (( $len > 2 )) ; then
+
+	echo 'found'
+
+fi
+
 
 echo "--------------------------------------"
 
