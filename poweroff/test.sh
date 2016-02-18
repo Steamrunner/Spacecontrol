64a092dea89b5522095898171d0aba066d8341f3
Steamrunner <koen@devoegt.be>
2016-02-18 21:16:36 +0100

    update

--- poweroff/test.sh
+++ poweroff/test.sh
@@ -1,3 +1,20 @@
-user=$(who | grep '(:[0-9])')
-readarray -t y <<<"$user"
+#!/bin/bash
+
+whoResults=$(who | grep '(:[0-9])')
+readarray -t whoResultLines <<< "$whoResults"
+
+echo "--------------------------------------"
+
+len=${#whoResultLines[@]}
+
+for (( i=0; i<${len}; i++ ));
+
+do
+	set -- ${whoResultLines[$i]}
+	user=$1
+	display=$5
+	echo $user $display
+done
+
+echo "--------------------------------------"
 
