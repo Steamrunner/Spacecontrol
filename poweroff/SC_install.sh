5dd67428dc520bc4206afc198c6d66838120fe86
Steamrunner <koen@devoegt.be>
2017-09-01 01:24:17 +0200

    starting an installation script

---
+++ poweroff/SC_install.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+# generate random password
+password=$(date +%s | sha256sum | base64 | head -c 32)
+echo $password
+
+# encode password
+encryptedPassword=$(mkpasswd -m sha-512 $password)
+echo "------------------------------------"
+echo $encryptedPassword
+echo "------------------------------------"
+
+
+
+# create user
+useradd -m -p $encryptedPassword -s /bin/bash shutdownuser
+
+exit 0
+
+# install some needed and some usefull stuff
+apt install git screen vim
+
+# 
+cd /home/shutdownuser
+git clone 
+# user priviliges???
+
+# ssh to spacecontrol server
+
+# copy the key form server to new client
+# generate line to be added to openhab config file
