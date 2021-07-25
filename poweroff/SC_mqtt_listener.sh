ae16bba41a17b9f2b5b90d109dd34ce8b2dc44d2
Steamrunner <koen@devoegt.be>
2021-07-25 20:37:13 +0200

    added logging

--- poweroff/SC_mqtt_listener.sh
+++ poweroff/SC_mqtt_listener.sh
@@ -1,9 +1,12 @@
 #!/usr/bin/env bash
 
-# get command line options
+# set defaults
+host=localhost
+port=1883
 config=poweroff.conf
 log=poweroff.log
 
+# get command line options
 while getopts c:l:h flag
 do
     case "${flag}" in
@@ -17,22 +20,29 @@ do
 done
 echo "`date`      MQTT Poweroff listener started" >> $log
 
-# load configuration file
-source $config
-echo "`date`      Configuration loaded" >> $log
+# load configuration file if available
+source $config 2>/dev/null
+configLoaded=$? 
+if [ $configLoaded -eq 0 ]
+then
+	echo "`date`      Configuration file loaded" >> $log
+else
+	echo "`date`      Unable to load configuration file, using defaults" >> $log
+fi
 
+# get local variables
 hostname=$(cat /etc/hostname)
-topic=$(echo "computers/$hostname")
+topic=$(echo "$topic_prefix/computers/$hostname")
+echo $topic
 
-msg=$(mosquitto_sub -h $host -p $port -u $username -P $password -C 1 -t "$topic")
-while [ "$msg" != "poweroff" ]
+# wait for incomming messages
+while :
 do
 	msg=$(mosquitto_sub -h $host -p $port -u $username -P $password -C 1 -t "$topic")
+	echo $msg
+	if [ "$msg" == "poweroff" ]
+	then
+		echo "`date`      Starting poweroff script" >> $log
+		$(dirname "$0")/SC_poweroff.sh
+	fi
 done
-
-echo "`date`      Start poweroff window script" >> $log
-$(dirname "$0")/SC_poweroff.sh
-
-echo "shutdown"
-
-# TODO continue loop if poweroff aborted
